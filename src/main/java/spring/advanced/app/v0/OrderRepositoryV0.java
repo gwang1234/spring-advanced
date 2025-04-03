60d063fc4afb988578eb9d47fed781795a1ee53a
gwang1234 <qoi11@naver.com>
2025-04-03 16:25:15 +0900

    간단 예제

---
+++ src/main/java/spring/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package spring.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        //저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("에외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) { // 현재 실행중인 스레드에게 작업중단 요청
+            e.printStackTrace();
+        }
+    }
+}
