4f38d60ffecdee508ff4dc570aada7989bad4e50
gwang1234 <qoi11@naver.com>
2025-04-04 16:34:37 +0900

    ThreadLocal

---
+++ src/test/java/spring/advanced/thredlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,41 @@
+package spring.advanced.thredlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.advanced.thredlocal.code.ThreadLocalService;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            threadLocalService.logic("userA");
+        };
+        Runnable userB = () -> {
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(2000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
