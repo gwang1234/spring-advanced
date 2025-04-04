dfe5e04dc7ef845104ac8e908ec492ee0c0dc80e
gwang1234 <qoi11@naver.com>
2025-04-04 16:13:45 +0900

    필드 동기화 - 동시성 문제

---
+++ src/test/java/spring/advanced/thredlocal/FieldServiceTest.java
@@ -0,0 +1,41 @@
+package spring.advanced.thredlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.advanced.thredlocal.code.FieldService;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(2000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
