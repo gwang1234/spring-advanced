f3d25468eb2d8aeb823b92b54125ead4970117c6
qoi11 <qoi11@naver.com>
2025-04-20 16:09:11 +0900

    전략 패턴

---
+++ src/test/java/spring/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,43 @@
+package spring.advanced.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.strategy.code.strategy.*;
+
+@Slf4j
+public class ContextV2Test {
+
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    // 전략패턴 - 익명 내부 클래스
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    // 전략패턴 - 람다
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+}
